6ad3c063923400ee3d2f09c8bfb0c456ad9cda4f
OkiriaPius <okiriapiusicloud@gmail.com>
2024-10-04 08:23:24 +0300

    adding the files index.html, script.js, and style.css

---
+++ scrip.js
@@ -0,0 +1,7 @@
+/* ---Create a new Git repository for your project.
+Create a blank HTML document with a script tag.
+Check if JavaScript is linked correctly:
+Write console.log("Hello World") in JavaScript.
+Check if “Hello World” is logged in the browser console once you open your webpage.
+--------- */
+console.log("Hello World");
